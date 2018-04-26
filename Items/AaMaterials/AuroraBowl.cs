e60b5aabeeb908aec88d08007393afc9d55a5145
terrynmuse <37078944+terrynmuse@users.noreply.github.com>
2018-04-26 15:59:33 -0400

    Add files via upload

---
+++ Items/AaMaterials/AuroraBowl.cs
@@ -0,0 +1,49 @@
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Terraria;
+using Terraria.ID;
+using Terraria.ModLoader;
+
+namespace ForgottenMemories.Items.AaMaterials
+{
+	public class AuroraBowl : ModItem
+	{
+		public override void SetDefaults()
+		{
+			item.width = 14;
+			item.height = 18;
+			item.maxStack = 999;
+			item.value = 500;
+			item.rare = 2;
+		}
+
+		public override void SetStaticDefaults()
+		{
+			DisplayName.SetDefault("Aurora Bowl");
+			Tooltip.SetDefault("'Whole beauty of sky and underground combined into one'");
+			BTFAGlowmask.AddGlowMask(item.type, "ForgottenMemories/Items/AaMaterials/AuroraBowlGlow");
+		}
+
+		public override void PostDrawInWorld(SpriteBatch spriteBatch, Color lightColor, Color alphaColor, float rotation, float  scale, int whoAmI) 	
+		{
+			Texture2D texture;
+			texture = Main.itemTexture[item.type];
+			spriteBatch.Draw
+			(
+				mod.GetTexture("Items/AaMaterials/AuroraBowlGlow"),
+				new Vector2
+				(
+					item.position.X - Main.screenPosition.X + item.width * 0.5f,
+					item.position.Y - Main.screenPosition.Y + item.height - texture.Height * 0.5f + 2f
+				),
+				new Rectangle(0, 0, texture.Width, texture.Height),
+				Color.White,
+				rotation,
+				texture.Size() * 0.5f,
+				scale, 
+				SpriteEffects.None, 
+				0f
+			);
+		}
+	}
+}
